13056c6cb38e692ba3664c47003bb43cb740e4db
ChibyzTech <igwehwizzy@outlook.com>
2021-10-16 05:10:11 +0100

    implementing test app

---
+++ app.js
@@ -0,0 +1,23 @@
+(function (){
+'use strict';
+
+    angular.module('SpaApp', [])
+    .controller('SpaAppController',function($scope){
+      $scope.name="";
+      $scope.totalvalue="0";
+      $scope.displaynumeric=function(){
+          var totalnamevalue = CalcNumericString($scope.name);//get the total value
+          $scope.totalvalue = totalnamevalue;
+      };
+      function CalcNumericString(string){
+          var totalstringvalue = 0;
+          for(var i = 0; i<string.length; i++){
+              totalstringvalue += string.charCodeAt(i);
+          }
+          return totalstringvalue;
+
+
+      };
+    });
+}
+)();
